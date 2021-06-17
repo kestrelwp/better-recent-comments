05384f38ba503f2aba0b52590533209c2b783b98
Andrew Keith <andy@barn2.co.uk>
2021-06-17 18:53:29 +0100

    Code standards cleanup

--- better-recent-comments.php
+++ better-recent-comments.php
@@ -3,7 +3,7 @@
  * The main plugin file for Better Recent Comments.
  *
  * @package   Barn2\better-recent-comments
- * @author    Barn2 Plugins <support@barn2.co.uk>
+ * @author    Barn2 Plugins <support@barn2.com>
  * @license   GPL-3.0
  * @copyright Barn2 Media Ltd
  *
@@ -11,9 +11,9 @@
  * Plugin Name:     Better Recent Comments
  * Plugin URI:      https://wordpress.org/plugins/better-recent-comments/
  * Description:     This plugin provides an improved widget and shortcode to show your most recent comments. If using WPML, comments are limited to posts in the current language.
- * Version:         1.1.4
+ * Version:         1.1.5
  * Author:          Barn2 Plugins
- * Author URI:      https://barn2.co.uk
+ * Author URI:      https://barn2.com
  * Text Domain:     better-recent-comments
  * Domain Path:     /languages
  *
@@ -21,22 +21,27 @@
  * License:         GNU General Public License v3.0
  * License URI:     https://www.gnu.org/licenses/gpl.html
  */
+
 namespace Barn2\Plugin\Better_Recent_Comments;
 
 // Prevent direct file access
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
-const PLUGIN_VERSION = '1.1.4';
+const PLUGIN_VERSION = '1.1.5';
 const PLUGIN_FILE    = __FILE__;
 
 // Include autoloader.
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Helper function to access the shared plugin instance.
+/**
+ * Helper function to access the shared plugin instance.
+ *
+ * @return Plugin The plugin instance.
+ */
 function better_recent_comments() {
-    return Plugin_Factory::create( PLUGIN_FILE, PLUGIN_VERSION );
+	return Plugin_Factory::create( PLUGIN_FILE, PLUGIN_VERSION );
 }
 
 // Load the plugin
--- src/Plugin_Factory.php
+++ src/Plugin_Factory.php
@@ -6,19 +6,19 @@ namespace Barn2\Plugin\Better_Recent_Comments;
  * Factory to create/return the shared plugin instance.
  *
  * @package   Barn2\better-recent-comments
- * @author    Barn2 Plugins <support@barn2.co.uk>
+ * @author    Barn2 Plugins <support@barn2.com>
  * @license   GPL-3.0
  * @copyright Barn2 Media Ltd
  */
 class Plugin_Factory {
 
-    private static $plugin = null;
+	private static $plugin = null;
 
-    public static function create( $file, $version ) {
-        if ( null === self::$plugin ) {
-            self::$plugin = new Plugin( $file, $version );
-        }
-        return self::$plugin;
-    }
+	public static function create( $file, $version ) {
+		if ( null === self::$plugin ) {
+			self::$plugin = new Plugin( $file, $version );
+		}
+		return self::$plugin;
+	}
 
 }
